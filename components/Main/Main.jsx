f85aa73efa273afbd8029edc48b6399bffc05975
Georgii Dmitruk <118625048+EliotCampbell@users.noreply.github.com>
2024-01-09 15:48:43 +0400

    Test commercelayer sdk

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -3,6 +3,15 @@ import CarouselBlock from './CarouselBlock/CarouselBlock'
 import classes from './Main.module.css'
 import ItemsLine from './ItemsLine/ItemsLine'
 import { Brand, Product } from '@/models/models'
+import commercelayer from '@commercelayer/sdk'
+
+const organization = 'buy-this-2'
+const accessToken =
+  'eyJhbGciOiJIUzUxMiJ9.eyJvcmdhbml6YXRpb24iOnsiaWQiOiJPUk5NV0ZkYnZuIiwic2x1ZyI6ImJ1eS10aGlzLTIiLCJlbnRlcnByaXNlIjpmYWxzZX0sImFwcGxpY2F0aW9uIjp7ImlkIjoicHlhcWlnck9LcCIsImtpbmQiOiJpbnRlZ3JhdGlvbiIsInB1YmxpYyI6ZmFsc2V9LCJ0ZXN0Ijp0cnVlLCJleHAiOjE3MDQ4MDM5MjMsInJhbmQiOjAuOTc0NjQ0NDQ3MDg3MjkyOH0.2Htp0pOC5wfkX_5kEwcKq5Glk0FnSPHTeAsb5KRrPeGsX9r5eIZK2dXvDunILYUXlxiuPJA3YH6dlirX1cX5bA'
+
+const cl = commercelayer({ organization, accessToken })
+
+cl.skus.list().then(console.log)
 
 const Main = async () => {
   const highlights = await Product.findAll({
